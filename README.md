902b71e710bdef47ac2eced6ea4ece46d00efa0e
Tobias Waller <tobias.waller@capgemini.com>
2020-08-14 16:11:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+from: https://github.com/codesandbox-app/static-template.git
---
+++ app.js
@@ -0,0 +1,23 @@
+function rollDice() {
+  const dice = [...document.querySelectorAll(".die-list")];
+  dice.forEach(die => {
+    toggleClasses(die);
+    die.dataset.roll = getRandomNumber(1, 6);
+  });
+}
+
+function toggleClasses(die) {
+  die.classList.toggle("odd-roll");
+  die.classList.toggle("even-roll");
+}
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+document.getElementById("roll-button").addEventListener("click", rollDice);
+document
+  .querySelectorAll(".die-item")
+  .forEach(i => i.addEventListener("click", rollDice));
